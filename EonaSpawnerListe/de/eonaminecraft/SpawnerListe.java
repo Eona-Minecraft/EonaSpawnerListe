da9e46c7bdb92ca2e68911604d56a3d99799aa9f
Bloodrayne1995 <bloodrayne1995@live.de>
2015-09-17 18:05:49 +0200

    FinalVersion

--- EonaSpawnerListe/de/eonaminecraft/SpawnerListe.java
+++ EonaSpawnerListe/de/eonaminecraft/SpawnerListe.java
@@ -73,53 +73,73 @@ public class SpawnerListe extends JavaPlugin {
 	
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
 		boolean erg = false;
+		boolean done = false;
 		Player x = getPlayerOfName(sender.getName());
 		double balance = 0;
-		switch(label){
-		case "getSpawner":
+		switch(label.toLowerCase()){
+		case "getspawner":
+			erg = true;
 			if(ec.isEconInit()){
 				balance  = ec.getEconomy().getBalance(x);
 				if(balance >= cfg.getSpawnerpreis()){
-					if(isUserRegistred(x.getUniqueId())){
+					if(!isUserRegistred(x.getUniqueId())){
 						registerUser(x.getUniqueId());
 					}
 					if(getAnzahlSpawnerFromUUID(x.getUniqueId()) < cfg.getSpawnerlimit()){
 						ec.getEconomy().withdrawPlayer(x, cfg.getSpawnerpreis());
 						x.getInventory().addItem(new ItemStack(Material.MOB_SPAWNER,1));
 						addSpawnerToUuid(x.getUniqueId());
+						sender.sendMessage("1 Spawner gekauft!");
+						done = true;
 					}else{
 						sender.sendMessage("Du hast schon zu viele Spawner.");
 					}
 				}else{
-					sender.sendMessage("Du hast nicht genügend Geld! ");
-					sender.sendMessage("Was du hast:     " + String.format("%.2f", balance));
-					sender.sendMessage("Was du brauchst: " + cfg.getSpawnerpreis());
+					if(!done){
+						sender.sendMessage("Du hast nicht genügend Geld! ");
+						sender.sendMessage("Was du hast:     " + String.format("%.2f", balance));
+						sender.sendMessage("Was du brauchst: " + cfg.getSpawnerpreis());
+					}
 				}
 			}else{
 				sender.sendMessage("Konnte Befehl nicht ausführen. Das Economy-System ist nicht initialisiert!");
 			}
+			break;
+		case "getspawnerpreis":
 			erg = true;
-		case "getSpawnerPreis":
 			sender.sendMessage("Um einen Spawner zu kaufen brauchst du " + cfg.getSpawnerpreis() + " EONA-Cash!");
-			erg = true;
 		case "spawnerliste":
+			//sender.sendMessage(args[0]);
 			if(args.length > 0){
 				erg = true;
-				if(args[0] == "version"){
+				if(args[0].equals("version")){
 					sender.sendMessage("Spawnerliste Version: ");
+				}else if (args[0].equals("reload")){
+					conn.closeConnections();
+					cfg.readConfig(getConfig());
+					ec.initEconomy(this);
+					conn.setConnectionData(getConnectionDataFromConfig());
+					conn.openConnections();
+					sender.sendMessage("Reload erfolgreich");
 				}else if(args.length > 1){
 					try{
 						Player p = getPlayerOfName(args[1]);
 						if(p == null){
 							throw new Exception("Spieler '" + args[1] + "' nicht gefunden");
 						}
+						if(!isUserRegistred(p.getUniqueId())){
+							registerUser(p.getUniqueId());
+						}
 						switch(args[0]){
 						case "inc":
-							if(getAnzahlSpawnerFromUUID(p.getUniqueId()) == cfg.getSpawnerlimit()){
+//							sender.sendMessage("DB:" + getAnzahlSpawnerFromUUID(p.getUniqueId()));
+//							sender.sendMessage("LIMIT :" +cfg.getSpawnerlimit());
+							if(getAnzahlSpawnerFromUUID(p.getUniqueId()) < cfg.getSpawnerlimit()){
 								addSpawnerToUuid(p.getUniqueId());
+								p.getInventory().addItem(new ItemStack(Material.MOB_SPAWNER,1));
 								sender.sendMessage("1 Spawner an Spieler '" + p.getDisplayName() + "' gegeben");
 							}else{
-								sender.sendMessage("Spieler '" + p.getDisplayName() + "' hat schon zu viele Spawner gegeben");
+								sender.sendMessage("Spieler '" + p.getDisplayName() + "' hat schon zu viele Spawner");
 							}
 							break;
 						case "dec":
@@ -133,6 +153,9 @@ public class SpawnerListe extends JavaPlugin {
 							deleteUUID(p.getUniqueId());
 							sender.sendMessage("Spieler '" + p.getDisplayName() + "' aus der SpawnerListe entfernt");
 							break;
+						default:
+							sender.sendMessage("Keine oder zu wenige Argumente!");
+							sender.sendMessage("Syntax: /spawnerliste <version|dec|inc|show|reload> [playername]");
 						}
 					}catch(Exception e){
 						sender.sendMessage(e.getMessage());
@@ -190,24 +213,28 @@ public class SpawnerListe extends JavaPlugin {
 	
 	
 	private void addSpawnerToUuid(UUID id){
-		boolean erg = isUserRegistred(id);
-		if(!erg){
-			registerUser(id);
+		if(!(getAnzahlSpawnerFromUUID(id) >= cfg.getSpawnerlimit())){
+			conn.execute(UPDATE_ADD_SPAWNER.replace("%uuid%", id.toString()));
 		}
-		conn.execute(UPDATE_ADD_SPAWNER.replace("%uuid%", id.toString()));
 	}
 	
 	
 	private void registerUser(UUID id){
-		conn.execute(CREATE_NEW_ENTRY.replace("%uuid%", id.toString()));
+		try{
+			conn.execute(CREATE_NEW_ENTRY.replace("%uuid%", id.toString()));
+		}catch(Exception e){
+			l.info(e.getMessage());
+//			l.info("SQL: " + CREATE_NEW_ENTRY.replace("%uuid%", id.toString()));
+		}
 	}
 	
 	private boolean isUserRegistred(UUID id){
+//		l.info("SQL: " + SELECT_IS_REGISTRED.replace("%uuid%", id.toString()));
 		ResultSet x = conn.query(SELECT_IS_REGISTRED.replace("%uuid%", id.toString()));
 		boolean erg = false;
 		try {
 			while(x.next()){
-				if(x.getInt(0) > 0){
+				if(x.getInt(1) > 0){
 					erg = true;
 				}
 			}
@@ -218,24 +245,21 @@ public class SpawnerListe extends JavaPlugin {
 	}
 	
 	private void decSpawnerToUuid(UUID id){
-		boolean erg = isUserRegistred(id);
-		if(!erg){
-			registerUser(id);
-		}
 		if(!(getAnzahlSpawnerFromUUID(id) <= 0)){
 			conn.execute(UPDATE_DEC_SPAWNER.replace("%uuid%", id.toString()));
 		}
 	}
 	
 	private int getAnzahlSpawnerFromUUID(UUID id){
+//		l.info(SELECT_GET_SPAWNER.replace("%uuid%", id.toString()));
 		ResultSet x = conn.query(SELECT_GET_SPAWNER.replace("%uuid%", id.toString()));
 		int erg = -1;
 		try{
 			while(x.next()){
-				erg = x.getInt(0);
+				erg = x.getInt(1);
 			}
 		}catch(Exception e){
-			
+			l.info(e.getMessage());
 		}
 		return erg;
 	}
